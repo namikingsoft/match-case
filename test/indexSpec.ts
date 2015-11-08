0851fb7d64a7ea45875d1ab4f44e80a36c11576c
Tsubasa Namiki <namikingsoft@gmail.com>
2015-11-09 02:10:49 +0900

    Add spec

--- test/indexSpec.ts
+++ test/indexSpec.ts
@@ -1,5 +1,5 @@
 import match from 'index'
-import Case from 'index'
+import {List} from 'immutable'
 
 describe("match-case", function() {
 
@@ -45,6 +45,55 @@ describe("match-case", function() {
       })
       .get()
       .should.be.equals(-10)
+    })
+  })
+
+  context("case 3", () => {
+    before(() => {
+      this.result = List.of<any>(
+        0, 1, "2", 3, ()=>4, new Date(), {num:6}, 7, undefined, 8,
+        "9", 10, "Hello", 13, 12, null, "11", 14, "World", 15, 16
+      )
+      .map<number>(
+        v => match<any, number>(v).noneThen(0)
+        .add({
+          when: n => typeof(n) === "number",
+          then: v => v
+        })
+        .add({
+          when: n => typeof(n) === "string" && /^[0-9]+$/.test(n),
+          then: v => parseInt(v)
+        })
+        .add({
+          when: n => typeof(n) === "function",
+          then: v => v()
+        })
+        .add({
+          when: n => n instanceof Date,
+          then: 5
+        })
+        .add({
+          when: n => n.num > 0,
+          then: v => v.num
+        })
+        .get()
+      )
+      .filter(n => (1 <= n&&n <= 15))
+      .sort()
+      .map<string>(
+        v => match<number, string>(v)
+        .whenThen(n => n%3===0 && n%5===0, "FizzBuzz")
+        .whenThen(n => n%3===0, "Fizz")
+        .whenThen(n => n%5===0, "Buzz")
+        .elseThen(v => v.toString())
+        .end()
+      )
+      .join(" ")
+    })
+    it("should be return correctly results", () => {
+      this.result.should.be.equal(
+        "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz"
+      )
     })
   })
 })
