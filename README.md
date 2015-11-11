4a22dd81a1e98a91b7afed96bd8c7a5c8f558ff6
Tsubasa Namiki <namikingsoft@gmail.com>
2015-11-11 12:51:43 +0900

    Update README usage and specs

--- README.md
+++ README.md
@@ -24,29 +24,29 @@ In case of ECMAScript6
 ```javascript
 import match from 'match-case'
 
-var result1 = match(10)
-  .caseOf(n => n > 0, v => v * v)
-  .caseOfElse(404)
-.end()
+const result1 = match(10).
+  caseOf(n => n > 0, v => v * v).
+  caseOfElse(404).
+end()
 
 assert(result1 === 100)
 
-var result2 = match(-1)
-  .caseOf({
+const result2 = match(-1).
+  caseOf({
     when: n => n > 0,
     then: v => v * v
-   })
-  .caseOfElse({
+   }).
+  caseOfElse({
     then: v => 404
-  })
-.get()
+  }).
+get()
 
 assert(result2 === 404)
 
-const matcher = match()
-  .caseOf(n => n > 0, 200)
-  .caseOfNone(404)
-  .caseOfElse(500)
+const matcher = match().
+  caseOf(n => n > 0, 200).
+  caseOfNone(404).
+  caseOfElse(500)
 
 assert(matcher.get(10) === 200)
 assert(matcher.get(-1) === 404)
@@ -59,13 +59,13 @@ In case of ECMAScript5
 ```javascript
 var match = require('match-case').default
 
-var result = match(10)
-  .caseOf({
+var result = match(10).
+  caseOf({
     when: function(n) {return n > 0},
     then: function(v) {return v * v}
-  })
-  .caseOfElse(404)
-.end()
+  }).
+  caseOfElse(404).
+end()
 
 assert(result === 100)
 ```
@@ -83,53 +83,53 @@ import {List} from 'immutable'
 const result = List.of<any>(
   0, 1, "2", 3, ()=>4, {num:5}, new Date(), 7, undefined, 8,
   "9", 10, "Hello", 11, "12", null, 13, 14, "World", 15, 16
-)
-.map<number>(
-  v => match<number>(v)
-  .caseOfNone(0) // prevent runtime error
-  .caseOf({
+).
+map<number>(
+  v => match<number>(v).
+  caseOfNone(0). // prevent runtime error
+  caseOf({
     when: n => typeof(n) === "number",
     then: v => v
-  })
-  .caseOf({
+  }).
+  caseOf({
     when: n => typeof(n) === "string" && /^[0-9]+$/.test(n),
     then: v => parseInt(v)
-  })
-  .caseOf({
+  }).
+  caseOf({
     when: n => typeof(n) === "function",
     then: v => v()
-  })
-  .caseOf({
+  }).
+  caseOf({
     when: n => !isNaN(n.num),
     then: v => parseInt(v.num)
-  })
-  .caseOf({
+  }).
+  caseOf({
     when: n => n instanceof Date,
     then: 6
-  })
-  .end()
- )
-.filter(n => (1 <= n&&n <= 15))
-.map<string>(
-  v => match<number,string>(v)
-  .caseOf({
+  }).
+  end()
+ ).
+filter(n => (1 <= n&&n <= 15)).
+map<string>(
+  v => match<number,string>(v).
+  caseOf({
     when: n => n%3===0 && n%5===0,
     then: "FizzBuzz"
-  })
-  .caseOf({
+  }).
+  caseOf({
     when: n => n%3===0,
     then: "Fizz"
-  })
-  .caseOf({
+  }).
+  caseOf({
     when: n => n%5===0,
     then: "Buzz"
-  })
-  .caseOfElse({
+  }).
+  caseOfElse({
     then: v => v.toString()
-  })
-  .end()
-)
-.join(" ")
+  }).
+  end()
+).
+join(" ")
 
 assert(
   result === "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz"
--- test/api/indexSpec.ts
+++ test/api/indexSpec.ts
@@ -7,75 +7,75 @@ describe("match-case", function() {
   context("basic", () => {
 
     it("can assign functions or value", () => {
-      const result1 = match<number,number>(10)
-      .caseOf(n => n > 0, v => v * v)
-      .get()
+      const result1 = match<number,number>(10).
+        caseOf(n => n > 0, v => v * v).
+      get()
       assert(result1 === 100)
 
-      const result2 = match<number,number>(10)
-      .caseOf(10, 100)
-      .end()
+      const result2 = match<number,number>(10).
+        caseOf(10, 100).
+      end()
       assert(result2 === 100)
 
-      const result3 = match<number,number>(-5)
-      .caseOf(n => n > 0, 100)
-      .caseOfElse(v => v + v)
-      .end()
+      const result3 = match<number,number>(-5).
+        caseOf(n => n > 0, 100).
+        caseOfElse(v => v + v).
+      end()
       assert(result3 === -10)
 
-      const result4 = match<number,number>(undefined)
-      .caseOf(n => n > 0, v => v * v)
-      .caseOfNone(v => 404)
-      .end()
+      const result4 = match<number,number>(undefined).
+        caseOf(n => n > 0, v => v * v).
+        caseOfNone(v => 404).
+      end()
       assert(result4 === 404)
 
-      const result5 = match<number,number>(null)
-      .caseOf(n => n > 0, v => v * v)
-      .caseOfNone(404)
-      .end()
+      const result5 = match<number,number>(null).
+        caseOf(n => n > 0, v => v * v).
+        caseOfNone(404).
+      end()
       assert(result5 === 404)
     })
 
     it("can assign object assigned functions or value", () => {
-      const result1 = match<number,number>(10)
-      .caseOf({
+      const result1 = match<number,number>(10).
+      caseOf({
         when: n => n > 0,
         then: v => v * v
-      })
-      .end()
+      }).
+      end()
       assert(result1 === 100)
 
-      const result2 = match<number,number>(10)
-      .caseOf({when: 10, then: 100})
-      .end()
+      const result2 = match<number,number>(10).
+        caseOf({when: 10, then: 100}).
+      end()
       assert(result2 === 100)
 
-      const result3 = match<number,number>(-5)
-      .caseOf({when: n => n > 0, then: 100})
-      .caseOfElse({then: v => v + v})
-      .end()
+      const result3 = match<number,number>(-5).
+        caseOf({when: n => n > 0, then: 100}).
+        caseOfElse({then: v => v + v}).
+      end()
       assert(result3 === -10)
 
-      const result4 = match<number,number>(undefined)
-      .caseOf({when: n => n > 0, then: v => v * v})
-      .caseOfNone({then: v => 404})
-      .end()
+      const result4 = match<number,number>(undefined).
+        caseOf({when: n => n > 0, then: v => v * v}).
+        caseOfNone({then: v => 404}).
+      end()
       assert(result4 === 404)
 
-      const result5 = match<number,number>(null)
-      .caseOf({when: n => n > 0, then: v => v * v})
-      .caseOfNone({then: v => 404})
-      .end()
+      const result5 = match<number,number>(null).
+      caseOf({when: n => n > 0, then: v => v * v}).
+      caseOfNone({then: v => 404}).
+      end()
       assert(result5 === 404)
     })
 
     it("can set value after case construction", () => {
-      const matcher = match<number,number>()
-      .caseOf({
+      const matcher = match<number,number>().
+      caseOf({
         when: n => n > 0,
         then: v => v * v
-      })
-      .caseOfElse({
+      }).
+      caseOfElse({
         then: v => v + v
       })
       assert(matcher.get(10) === 100)
@@ -83,12 +83,12 @@ describe("match-case", function() {
     })
 
     it("should be return undefined if unmatch without caseOfElse", () => {
-      const result = match<number,number>(-1)
-      .caseOf({
+      const result = match<number,number>(-1).
+      caseOf({
         when: n => n > 0,
         then: v => v * v
-      })
-      .end()
+      }).
+      end()
       assert(result === undefined)
     })
   })
@@ -99,53 +99,53 @@ describe("match-case", function() {
       const result = List.of<any>(
         0, 1, "2", 3, ()=>4, {num:5}, new Date(), 7, undefined, 8,
         "9", 10, "Hello", 11, "12", null, 13, 14, "World", 15, 16
-      )
-      .map<number>(
-        v => match<number>(v)
-        .caseOfNone(0) // prevent runtime error
-        .caseOf({
+      ).
+      map<number>(
+        v => match<number>(v).
+        caseOfNone(0). // prevent runtime error
+        caseOf({
           when: n => typeof(n) === "number",
           then: v => v
-        })
-        .caseOf({
+        }).
+        caseOf({
           when: n => typeof(n) === "string" && /^[0-9]+$/.test(n),
           then: v => parseInt(v)
-        })
-        .caseOf({
+        }).
+        caseOf({
           when: n => typeof(n) === "function",
           then: v => v()
-        })
-        .caseOf({
+        }).
+        caseOf({
           when: n => !isNaN(n.num),
           then: v => parseInt(v.num)
-        })
-        .caseOf({
+        }).
+        caseOf({
           when: n => n instanceof Date,
           then: 6
-        })
-        .end()
-       )
-      .filter(n => (1 <= n&&n <= 15))
-      .map<string>(
-        v => match<number,string>(v)
-        .caseOf({
+        }).
+        end()
+      ).
+      filter(n => (1 <= n&&n <= 15)).
+      map<string>(
+        v => match<number,string>(v).
+        caseOf({
           when: n => n%3===0 && n%5===0,
           then: "FizzBuzz"
-        })
-        .caseOf({
+        }).
+        caseOf({
           when: n => n%3===0,
           then: "Fizz"
-        })
-        .caseOf({
+        }).
+        caseOf({
           when: n => n%5===0,
           then: "Buzz"
-        })
-        .caseOfElse({
+        }).
+        caseOfElse({
           then: v => v.toString()
-        })
-        .end()
-      )
-      .join(" ")
+        }).
+        end()
+      ).
+      join(" ")
 
       assert(
         result === "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz"
